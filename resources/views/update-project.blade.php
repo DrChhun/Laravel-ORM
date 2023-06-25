313969a448283dc89f143fc861f1a29ad0b4ddcf
chhunkim394 <chhunkim394@gmail.com>
2023-06-25 22:49:25 +0700

    finshed information

--- resources/views/update-project.blade.php
+++ resources/views/update-project.blade.php
@@ -4,29 +4,34 @@
     <form class="flex flex-col w-1/2 mx-auto" action="/projects/{{$id}}" method="post">
         {{ method_field('PUT') }}
         @csrf
-        <label for="">Title</label>
-        <input class="border-2 border-gray-200" name="title" type="text" placeholder="title">
+        @foreach($data as $data)
+            <label for="">Title</label>
+            <input value="{{$data->title}}" class="border-2 border-gray-200" name="title" type="text" placeholder="title">
 
-        <label for="">Descripition</label>
-        <textarea placeholder="text here" class="border-2 border-gray-200" name="description" id="" cols="30" rows="10">
-        </textarea>
+            <label for="">Descripition</label>
+            <textarea placeholder="text here" class="border-2 border-gray-200" name="description" id="" cols="30" rows="10">
+                {{$data->description}}
+            </textarea>
 
-        <label for="">Deadline</label>
-        <input class="border-2 border-gray-200" name="deadline" type="date" placeholder="deadline">
+            <label for="">Deadline</label>
+            <input value="{{$data->deadline}}" class="border-2 border-gray-200" name="deadline" type="date" placeholder="deadline">
 
-        <label for="">Assigned client</label>
-        <select class="border-2 border-gray-200" name="assigned_client" id="cars">
-            @foreach($client as $client)
-                <option value="{{$client->id}}">{{$client->company}}</option>
-            @endforeach
-        </select>
+            <label for="">Assigned client</label>
+            <select class="border-2 border-gray-200" name="assigned_client" id="cars">
+                <option value="{{$data->assigned_client}}" selected hidden>{{$client[$data->assigned_client -1]->company}}</option>
+                @foreach($client as $client)
+                    <option value="{{$client->id}}">{{$client->company}}</option>
+                @endforeach
+            </select>
 
-        <label for="">Status</label>
-        <select class="border-2 border-gray-200" name="status" id="cars">
-            <option value="Open">Open</option>
-            <option value="Close">Close</option>
-        </select>
+            <label for="">Status</label>
+            <select class="border-2 border-gray-200" name="status" id="cars">
+                <option value="{{$data->status}}" selected hidden>{{$data->status}}</option>
+                <option value="Open">Open</option>
+                <option value="Close">Close</option>
+            </select>
 
-        <button class="text-start bg-blue-500 px-8 py-2 w-fit text-white rounded-md" type="submit">Submit</button>
+            <button class="text-start bg-blue-500 px-8 py-2 w-fit text-white rounded-md" type="submit">Submit</button>
+        @endforeach
     </form>
 @endsection
